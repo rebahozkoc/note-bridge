aeb92aecd2e372c069ae3c29d1f62657f4bb5351
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2024-05-29 00:51:05 +0200

    Fixed the project structure

--- src/main/java/nl/utwente/di/NoteBridge/Quoter.java
+++
@@ -1,8 +0,0 @@
-package nl.utwente.di.NoteBridge;
-
-public class Quoter {
-    public double getBookPrice(String isbn){
-        double i=Double.parseDouble(isbn);
-        return ((i * 9) / 5) + 32;
-    }
-}

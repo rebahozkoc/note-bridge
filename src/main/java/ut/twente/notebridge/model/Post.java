0a68c8bbe8c0f4d13af5f9391b75edf8bcc57e99
Anil <a.sen-1@student.utwente.nl>
2024-06-01 15:49:26 +0200

    updates on Post class and UserRoute

--- src/main/java/ut/twente/notebridge/model/Post.java
+++ src/main/java/ut/twente/notebridge/model/Post.java
@@ -1,14 +1,20 @@
 package ut.twente.notebridge.model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Post extends BaseEntity {
 
+	//owner needs to be added
 	private String title;
-
-
-	public Post() {
-		super();
-		title = null;
-	}
+	private String description;
+	private int likeCount;
+	private int commentCount;
+	private String sponsoredBy; // Optional
+	private List<String> pictures; // Assuming pictures are stored as URLs or file paths
+	private String eventType;
+	private String location;
+	private String message;
 
 	public String getTitle() {
 		return title;
@@ -16,5 +22,83 @@ public class Post extends BaseEntity {
 
 	public void setTitle(String title) {
 		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getLikeCount() {
+		return likeCount;
+	}
+
+	public void setLikeCount(int likeCount) {
+		this.likeCount = likeCount;
+	}
+
+	public int getCommentCount() {
+		return commentCount;
+	}
+
+	public void setCommentCount(int commentCount) {
+		this.commentCount = commentCount;
+	}
+
+	public String getSponsoredBy() {
+		return sponsoredBy;
+	}
+
+	public void setSponsoredBy(String sponsoredBy) {
+		this.sponsoredBy = sponsoredBy;
+	}
+
+	public List<String> getPictures() {
+		return pictures;
+	}
+
+	public void setPictures(List<String> pictures) {
+		this.pictures = pictures;
+	}
+
+	public String getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Post() {
+		super();
+		title=null;
+		description=null;
+		likeCount=0;
+		commentCount=0;
+		sponsoredBy=null;
+		pictures=null;
+		eventType=null;
+		location=null;
+		message=null;
+
 	}
 }
--- src/main/java/ut/twente/notebridge/routes/UserRoute.java
+++ src/main/java/ut/twente/notebridge/routes/UserRoute.java
@@ -1,4 +1,23 @@
 package ut.twente.notebridge.routes;
 
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.core.MediaType;
+import ut.twente.notebridge.dao.UserDao;
+import ut.twente.notebridge.model.User;
+
+@Path("/users")
 public class UserRoute {
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User getUser(@PathParam("id") String id) {
+        return UserDao.INSTANCE.getUser(id);
+    }
+
 }

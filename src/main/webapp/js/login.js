5c547597bfead53207e8d1953c71ba366d5c7ad8
AlexEnescu <a.enescu@student.utwente.nl>
2024-06-12 02:50:00 +0200

    send request to create account

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -2,9 +2,15 @@ const mainContent = document.querySelector('#main-content');
 
 function showRegisterForm() {
     mainContent.innerHTML = `
-    <form class="container main-content w-50 rounded-5 p-5" onsubmit="">
+    <form id="register-form" class="container main-content mt-5 w-50 rounded-5 p-5" onsubmit="return sendRequestCreateAccount()">
             <h1 class="fw-bold text-white py-2 fs-1">Sign up</h1>
             <div class="row">
+                <div class="col-6">
+                    <div class="form-floating mb-3">
+                        <textarea class="form-control" placeholder="Firstname" id="firstname" name="name"></textarea>
+                        <label for="firstname">First name</label>
+                    </div>
+                </div>
                 <div class="col-6">
                     <div class="form-floating mb-3">
                         <textarea class="form-control" placeholder="Lastname" id="lastname" name="lastname"></textarea>
@@ -12,15 +18,9 @@ function showRegisterForm() {
                     </div>
                 </div>
                 <div class="col-6">
-                    <div class="form-floating mb-3">
-                        <textarea class="form-control" placeholder="Firstname" id="firstname" name="firstname"></textarea>
-                        <label for="firstname">First name</label>
-                    </div>
-                </div>
-                <div class="col-6">
                     <div class="mb-3">
                         <label for="profilePicture" class="form-label text-white">Profile picture</label>
-                        <input class="form-control form-control-sm" type="file" id="profilePicture">
+                        <input class="form-control form-control-sm" type="file" id="profilePicture" name="picture">
                     </div>
                 </div>
                 <div class="col-6">
@@ -37,13 +37,13 @@ function showRegisterForm() {
                 </div>
                 <div class="col-12">
                     <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email">
                         <label for="floatingInput">Email address</label>
                     </div>     
                 </div>
                 <div class="col-6">
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" aria-describedby="passwordHelpBlock">
+                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" aria-describedby="passwordHelpBlock">
                         <label for="floatingPassword">Password</label>
                     </div>
                 </div>
@@ -60,4 +60,27 @@ function showRegisterForm() {
             <button type="submit" class="btn btn-outline-light px-3 py-2 rounded-4">Create account</button>
         </form>
     `;
+}
+
+function sendRequestCreateAccount() {
+    const form = document.getElementById("register-form");
+    const data = new FormData(form);
+    let dataObject = {};
+
+    for (const [key, value] of data.entries()) {
+        dataObject[key] = value;
+    }
+
+    console.log("test");
+    console.log(JSON.stringify(dataObject))
+
+    fetch("/notebridge/api/persons", {
+        method: "POST",
+        body: JSON.stringify(dataObject),
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    location.reload();
+    return false;
 }

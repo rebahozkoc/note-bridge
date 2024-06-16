390ca8414427c552f60ee6459ffd156f3d3614c7
AlexEnescu <a.enescu@student.utwente.nl>
2024-06-16 13:15:09 +0200

    log in form finished

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -1,3 +1,30 @@
+const form = document.querySelector("#login-form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    fetch("/notebridge/api/auth/login", {
+        method: "POST",
+        body: new URLSearchParams({
+            'email': document.getElementById("email").value,
+            'password': document.getElementById("password").value
+        }).toString(),
+        headers: {
+            "Content-type": 'application/x-www-form-urlencoded'}
+        }).then(res => {
+        if(res.status === 200) {
+            window.location.replace("http://localhost:8080/notebridge/")
+        } else {
+            res.text().then(data => {
+                const warningMessage = document.querySelector("#warning-message");
+                warningMessage.innerHTML = data;
+            });
+        }})
+        .catch(err => {
+        console.error("Log In Error", err)
+    })
+});
+
 function showRegisterUserForm() {
     const mainContent = document.querySelector('#main-content');
     mainContent.innerHTML = `

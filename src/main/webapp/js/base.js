27e37fd092d81e8bba5acc0d1fd10e2b2b8e32a2
AlexEnescu <a.enescu@student.utwente.nl>
2024-06-17 19:48:32 +0200

    home page finished

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -6,69 +6,6 @@ let cards = {};
 fetchPosts();
 displayAllCards();
 
-// function fetchPosts() {
-//     fetch('/notebridge/api/posts')
-//         .then(res => res.json())
-//         .then(data => {
-//             cards = data;
-//             displayAllCards();
-//         })
-//         .catch(err => {
-//             console.error(`Unable to fetch cards: ${err.status}`);
-//             console.error(err);
-//         });
-// }
-//
-// function displayAllCards() {
-//     posts.innerHTML = `
-//         <h1 class="fw-bold text-white fs-2 my-5">Browse posts</h1>
-//         ${cards.data.map(post => `${displayCard(post)}`).join("\n")}
-//     `;
-// }
-
-function displayCard(card) {
-    return `
-    <div class="m-5">
-            <div class="card card-background">
-                <div class="row g-0">
-                    <div class="col-md-7">
-                        <div class="card-body">
-                            <h5 class="card-title mb-4">${card.title}</h5>
-                            <p class="card-text">${card.description}</p>
-                        </div>
-                    </div>
-                    <div class="col-md-5">
-                        <img src="background2.png" class="img-fluid rounded-end" alt="card-image">
-                    </div>
-                </div>
-            </div>
-            <div class="mt-2 card-background rounded-3 py-1 px-1">
-                <button class="card-btn">&#128077;</button>
-                143
-                <button class="card-btn" type="button" data-bs-toggle="collapse" data-bs-target="#collapse" aria-expanded="false" aria-controls="collapse">
-                    &#128172;
-                </button>
-                34
-            </div>
-            <div class="collapse mt-2" id="collapse">
-                <div class="card card-body">
-                    <div class="form-floating mb-3">
-                        <textarea class="form-control" placeholder="Description" id="add-comment" style="height: 11vh"></textarea>
-                        <label for="add-comment">Add a comment</label>
-                        <button type="submit" class="btn btn-outline-light rounded-4 py-2 my-2">Send</button>
-                    </div>
-                    <div class="comment rounded-3 mb-2 p-2">
-                        <span>User 1</span> said:<br>Comment 1
-                    </div>
-                    <div class="comment rounded-3 mb-2 p-2">
-                        <span>User 2</span> said:<br>Comment 2
-                    </div>
-                </div>
-            </div>
-        </div>
-    `
-}
-
 function showCreatePostForm() {
     mainContent.innerHTML = `
     <form class="container main-content w-75 rounded-5 p-5" onsubmit="return sendRequestCreatePost()">
@@ -132,29 +69,6 @@ function sendRequestCreatePost() {
         displayAllCards();
     });
     return false;
-}
-
-function showContactUsForm() {
-    mainContent.innerHTML = `
-    <div class="container main-content w-75 rounded-5 p-5">
-        <h1 class="fw-bold text-white py-2 fs-2">Contact Us</h1>
-        <form>
-            <div class="mb-3">
-              <label for="exampleFormControlInput1" class="form-label text-white">Email address</label>
-              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-            </div>
-            <div class="mb-3">
-              <label for="exampleFormControlTextarea1" class="form-label text-white">Message</label>
-              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-            <a href="" class="btn btn-outline-light mt-1 px-3 py-2 rounded-4">Send message</a>
-        </form>
-        <footer class="footer mt-4">
-          <p class="text-white">&copy; 2024 NoteBridge © All rights reserved.</p>
-          <p class="text-white">Designed by UT students.</p>
-        </footer>
-    </div>
-    `;
 }
 
 function showPosts() {

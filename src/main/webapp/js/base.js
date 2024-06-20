9674d7e3be4f2d3e2ac558abf63ae824e2332559
AlexEnescu <a.enescu@student.utwente.nl>
2024-06-20 10:53:05 +0200

    Navbar fixed

--- src/main/webapp/js/base.js
+++ src/main/webapp/js/base.js
@@ -69,13 +69,19 @@ function updateNavbar(loggedIn) {
         logInBtn.innerHTML = `
         <a href="home.html" class="button-cover ms-2" role="button" onclick="logOut()"><span class="text px-2">Log out</span><span>Log out</span></a>
         `
-        messengerBtn.style.display = "block";
-        profileBtn.style.display = "block";
+
+        messengerBtn.innerHTML = `
+        <a href="Messenger.html" class="button-cover me-3" role="button"><span class="text px-5">Messenger</span><span>Messenger</span></a>
+        `;
+
+        profileBtn.innerHTML = `
+        <a class="nav-link" href="profile.html"><img src="assets/images/user-icon.png" width="35px" height="35px"></a>
+        `;
     } else {
         logInBtn.innerHTML = `
         <a href="login.html" class="button-cover ms-2" role="button"><span class="text px-2">Log in</span><span>Log in</span></a>
         `
-        messengerBtn.style.display = "none";
-        profileBtn.style.display = "none";
+        messengerBtn.innerHTML = ``;
+        profileBtn.innerHTML = ``;
     }
 }

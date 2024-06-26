c8314f472938e317d78d1c65919598a41fa7c523
Anil <a.sen-1@student.utwente.nl>
2024-06-26 23:21:23 +0200

    getComments is modified such that picture field is providing base64 encoding of the profile picture

--- src/main/java/ut/twente/notebridge/dao/CommentDao.java
+++ src/main/java/ut/twente/notebridge/dao/CommentDao.java
@@ -5,13 +5,20 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import org.apache.commons.io.FileUtils;
+import ut.twente.notebridge.dto.CommentDto;
 import ut.twente.notebridge.dto.CommentDtoList;
 import ut.twente.notebridge.model.Comment;
 import ut.twente.notebridge.utils.DatabaseConnection;
 import ut.twente.notebridge.utils.Security;
+import ut.twente.notebridge.utils.Utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 import java.time.Instant;
+import java.util.Base64;
 
 public enum CommentDao {
 
@@ -73,7 +80,21 @@ public enum CommentDao {
 			if (rs.next()) {
 				String json = rs.getString("jsonb_build_object");
 				ObjectMapper mapper = JsonMapper.builder().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true).build();
-				return mapper.readValue(json, CommentDtoList.class);
+				CommentDtoList listOfComments= mapper.readValue(json, CommentDtoList.class);
+				for (CommentDto comment : listOfComments.getComments()) {
+					String fileLocation = Utils.readFromProperties("PERSISTENCE_FOLDER_PATH") + comment.getPicture();
+					File file = new File(fileLocation);
+					if (file.exists()) {
+						try {
+							byte[] fileContent = FileUtils.readFileToByteArray(file);
+							String encodedString = Base64.getEncoder().encodeToString(fileContent);
+							comment.setPicture(encodedString);
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				return listOfComments;
 			} else {
 				throw new NotFoundException("No comments found for post with id " + id);
 			}
